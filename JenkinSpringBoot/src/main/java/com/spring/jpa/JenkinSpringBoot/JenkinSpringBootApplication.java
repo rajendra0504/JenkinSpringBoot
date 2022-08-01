2c93cdd853d2fda0c6593ba19285f763cc4e9ede
rajendra0504 <varaprasad0504@gmail.com>
2022-08-02 01:17:26 +0530

    Initial commit

---
+++ JenkinSpringBoot/src/main/java/com/spring/jpa/JenkinSpringBoot/JenkinSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.JenkinSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinSpringBootApplication.class, args);
+	}
+
+}
